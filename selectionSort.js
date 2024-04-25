0b459d7f33c990374097f26e37668e7273d07241
Rogerio Lima <rlimapg@gmail.com>
2024-04-25 18:30:36 -0300

    sordenar arrays

---
+++ selectionSort.js
@@ -0,0 +1,34 @@
+const livros = require('./listaLivros');
+const menorValor = require('./menorValor');
+const maiorValor = require('./maiorValor');
+
+// for (let atual = 0; atual < livros.length; atual++){
+//     let menor = menorValor(livros, atual)
+
+//     let livroAtual = livros[atual];
+//     let livrosMenorPreco = livros[menor];
+
+//     livros[atual] = livrosMenorPreco;
+//     console.log ('posiçao atual', atual);
+//     console.log('livro atual', livros[atual]);
+//     livros[menor] = livroAtual;
+//     console.log('livro menor preço', livros[menor]);
+
+// }
+//  console.log(livros);
+
+
+for(let atual = 0; atual < livros.length; atual ++){
+    let maior = maiorValor (livros, atual)
+
+
+    let livroAtual = livros[atual];
+    console.log ('posiçao atual', atual);
+    console.log('livro atual', livros[atual]);
+    livrosMaiorPreco = livros[maior];
+    console.log('livro maior preço', livros[maior]);
+
+    livros[atual] = livrosMaiorPreco;
+    livros[maior] = livroAtual;
+}
+console.log(livros);
